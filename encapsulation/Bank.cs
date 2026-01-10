cf91df997ff68769b36229f328ca13134f8a575d
vijaygla <vijaykumarmganj1@gmail.com>
2026-01-10 07:25:40 +0530

    encapsulation done

--- encapsulation/Bank.cs
+++ encapsulation/Bank.cs
@@ -1,11 +1,22 @@
 using System;
 
 class BankAccount {
-    
+    private int bankBalance;
+
+    public void Deposit(int amount) {
+        bankBalance += amount;
+    }
+
+    public int CheckBalance() {
+        return bankBalance;
+    }
 }
 
 class Bank {
     static void Main(String[] args) {
+        BankAccount bank = new BankAccount();
+        bank.Deposit(2000);
 
+        Console.WriteLine("Bank balance: " + bank.CheckBalance());
     }
 }
